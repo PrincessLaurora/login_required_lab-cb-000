48d4e603675461bb8f24518d98472a32a13b979d
Laura Felce <crepu83@yahoo.it>
2019-07-05 10:15:00 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,11 +2,11 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
-    
+
   end
 
   private
     redirect_to login_path unless current_user
-  end  
+  end
 
 end
