599253f33fe7e655d32aaccbc33667c995476618
Laura Felce <crepu83@yahoo.it>
2019-07-05 10:28:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
-end  
+
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to login_path
+    elsif
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end  
